0bdc366ef78078419ac07241da022d2778a25c09
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2024-03-25 17:21:33 +0530

    Fix Source File formatting using black

--- tclwrapper/__init__.py
+++ tclwrapper/__init__.py
@@ -1,4 +1,10 @@
 from tclwrapper.tclwrapper import *
-from tclwrapper.tclutil import tclstring_to_list, tclstring_to_nested_list, tclstring_to_flat_list, list_to_tclstring, nested_list_to_tclstring
+from tclwrapper.tclutil import (
+    tclstring_to_list,
+    tclstring_to_nested_list,
+    tclstring_to_flat_list,
+    list_to_tclstring,
+    nested_list_to_tclstring,
+)
 
-__name__ = 'tclwrapper'
+__name__ = "tclwrapper"
--- tclwrapper/tclutil.py
+++ tclwrapper/tclutil.py
@@ -1,20 +1,22 @@
 import tkinter
 
 # use tkinter because it already has some functions to parse tcl output
-_tcl = tkinter.Tk(useTk = 0)
+_tcl = tkinter.Tk(useTk=0)
+
 
 def tclstring_to_list(tclstring):
     ret = _tcl.tk.splitlist(tclstring)
     # always return the result as a tuple
     if isinstance(ret, str):
-        if ret == '':
+        if ret == "":
             return ()
         else:
             return (ret,)
     else:
         return ret
 
-def tclstring_to_nested_list(tclstring, levels = None):
+
+def tclstring_to_nested_list(tclstring, levels=None):
     """
     Parses the given Tcl string as a nested list.
 
@@ -27,7 +29,7 @@ def tclstring_to_nested_list(tclstring, levels = None):
         ret = [_tcl.tk.splitlist(x) for x in _tcl.tk.splitlist(tclstring)]
         # always return the result as a tuple
         if isinstance(ret, str):
-            if ret == '':
+            if ret == "":
                 return ()
             else:
                 return (ret,)
@@ -35,30 +37,40 @@ def tclstring_to_nested_list(tclstring, levels = None):
             return ret
     else:
         if levels < 0:
-            raise ValueError('levels must be non-negative')
+            raise ValueError("levels must be non-negative")
         elif levels == 0:
             return tclstring
         else:
-            return tuple([tclstring_to_nested_list(x, levels-1) for x in tclstring_to_list(tclstring)])
+            return tuple(
+                [
+                    tclstring_to_nested_list(x, levels - 1)
+                    for x in tclstring_to_list(tclstring)
+                ]
+            )
+
 
 def tclstring_to_flat_list(tclstring):
-    return tclstring.replace('{', ' ').replace('}', ' ').split()
+    return tclstring.replace("{", " ").replace("}", " ").split()
+
+
 def list_to_tclstring(in_list):
     cleaned_up_list = []
     for entry in in_list:
-        if ' ' in entry:
-            cleaned_up_list.append('{' + entry + '}')
+        if " " in entry:
+            cleaned_up_list.append("{" + entry + "}")
         else:
             cleaned_up_list.append(entry)
-    return ' '.join(cleaned_up_list)
+    return " ".join(cleaned_up_list)
+
+
 def nested_list_to_tclstring(nested_list):
     items = []
     for item in nested_list:
         if isinstance(item, str):
-            if ' ' in item:
-                items.append('{' + item + '}')
+            if " " in item:
+                items.append("{" + item + "}")
             else:
                 items.append(item)
         else:
-            items.append('{' + nested_list_to_tclstring(item) + '}')
-    return ' '.join(items)
+            items.append("{" + nested_list_to_tclstring(item) + "}")
+    return " ".join(items)
